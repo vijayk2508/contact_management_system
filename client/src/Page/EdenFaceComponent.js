a5c865f8927d1a0a62c3e9e15b5c221f7502dec4
Vijay Kumar <vijay.kumar@emvigotech.com>
2024-05-30 21:00:44 +0530

    FACE COMPARISON APP

--- client/src/Page/EdenFaceComponent.js
+++ client/src/Page/EdenFaceComponent.js
@@ -1,48 +1,20 @@
-import React, { useState, useRef, useEffect } from "react";
-import { face_comparison, uploadImages } from "../Services/edenServiceAPI";
+import React, { useState } from "react";
+import { face_comparison } from "../Services/edenServiceAPI";
+
+const images = [
+  "https://edenai.s3.eu-west-1.amazonaws.com/aman-gupta.jpg",
+  "https://edenai.s3.eu-west-1.amazonaws.com/new.jpg",
+  "https://edenai.s3.eu-west-1.amazonaws.com/test1.jpg",
+  "https://edenai.s3.eu-west-1.amazonaws.com/test2.jpg",
+];
 
 const EdenFaceComponent = () => {
-  const [image1, setImage1] = useState(
-    "https://edenai.s3.eu-west-1.amazonaws.com/WIN_20240529_13_00_01_Pro.jpg"
-  );
-  const [image2, setImage2] = useState(
-    "https://edenai.s3.eu-west-1.amazonaws.com/aman-gupta.jpg",
-   //"https://edenai.s3.eu-west-1.amazonaws.com/WIN_20240529_13_00_02_Pro.jpg"
-   "https://edenai.s3.eu-west-1.amazonaws.com/new.jpg"
-  );
-
+  const [image1, setImage1] = useState(null);
+  const [image2, setImage2] = useState(null);
   const [comparisonResult, setComparisonResult] = useState(null);
-  // const videoRef1 = useRef(null);
-  // const videoRef2 = useRef(null);
-
-  // const handleImageUpload = (setImage) => (event) => {
-  //   const file = event.target.files[0];
-  //   const reader = new FileReader();
-  //   reader.onload = () => {
-  //     setImage(reader.result);
-  //   };
-  //   reader.readAsDataURL(file);
-  // };
-
-  // const handleCaptureImage = (videoRef, setImage) => () => {
-  //   const video = videoRef.current;
-
-  //   if (video) {
-  //     const canvas = document.createElement("canvas");
-  //     canvas.width = video.videoWidth;
-  //     canvas.height = video.videoHeight;
-  //     const context = canvas.getContext("2d");
-  //     context.drawImage(video, 0, 0, canvas.width, canvas.height);
-
-  //     canvas.toBlob((blob) => {
-  //       const reader = new FileReader();
-  //       reader.onloadend = () => {
-  //         setImage(reader.result);
-  //       };
-  //       reader.readAsDataURL(blob);
-  //     }, "image/jpeg");
-  //   }
-  // };
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [selectedImageType, setSelectedImageType] = useState(null);
+  const [loading, setLoading] = useState(false); // Add loading state
 
   const handleCompareImages = async () => {
     if (!image1 || !image2) {
@@ -52,73 +24,148 @@ const EdenFaceComponent = () => {
       return;
     }
 
+    setLoading(true); // Set loading to true before making the comparison
+
     try {
-      const result = await face_comparison(image1,image2);
-      setComparisonResult(result.message || "Comparison successful");
+      const result = await face_comparison(image1, image2);
+      console.log(result?.amazon?.items?.[0]?.confidence);
+      setComparisonResult({
+        per:
+          Math.floor(result?.amazon?.items?.[0]?.confidence * 100000) / 1000 ||
+          0,
+        boundingBox: result?.amazon?.items?.[0]?.bounding_box || null,
+      });
     } catch (error) {
       setComparisonResult("Error comparing images: " + error.message);
+    } finally {
+      setLoading(false); // Set loading to false after comparison completes
     }
   };
 
-  // useEffect(() => {
-  //   const setupVideo = (videoRef) => {
-  //     navigator.mediaDevices
-  //       .getUserMedia({ video: true })
-  //       .then((stream) => {
-  //         if (videoRef.current) {
-  //           videoRef.current.srcObject = stream;
-  //         }
-  //       })
-  //       .catch((error) => {
-  //         console.error("Error accessing webcam: ", error);
-  //       });
-  //   };
+  const handleImageClick = (url) => {
+    setSelectedImage(url);
+  };
 
-  //   setupVideo(videoRef1);
-  //   setupVideo(videoRef2);
-  // }, []);
+  const handleSetImage = (url) => {
+    if (selectedImageType === "image1") {
+      setImage1(url);
+    } else if (selectedImageType === "image2") {
+      setImage2(url);
+    }
+    setSelectedImage(null); // Close the modal
+    setSelectedImageType(null); // Reset selected image type
+  };
 
   return (
-    <div>
-      <h1>Face Comparison App</h1>
-      {/* <div>
-        <h2>Upload Image 1</h2>
-        <input
-          type="file"
-          accept="image/*"
-          onChange={handleImageUpload(setImage1)}
-        />
-        <video
-          ref={videoRef1}
-          autoPlay
-          style={{ display: image1 ? "none" : "block" }}
-        />
-        <button onClick={handleCaptureImage(videoRef1, setImage1)}>
-          Capture from Camera
-        </button>
-        {image1 && <img src={image1} alt="Image 1" />}
+    <div className="main">
+      <div id="heading">
+        <h1>Face Comparison App</h1>
       </div>
-      <div>
-        <h2>Upload Image 2</h2>
-        <input
-          type="file"
-          accept="image/*"
-          onChange={handleImageUpload(setImage2)}
-        />
-        <video
-          ref={videoRef2}
-          autoPlay
-          style={{ display: image2 ? "none" : "block" }}
-        />
-        <button onClick={handleCaptureImage(videoRef2, setImage2)}>
-          Capture from Camera
-        </button>
-        {image2 && <img src={image2} alt="Image 2" />}
-      </div> */}
-      {image1 && <img style={{height : "100px"}} src={image1} alt="Image 1" />}
-      {image2 && <img style={{height : "100px"}} src={image2} alt="Image 2" />}
-      <button onClick={handleCompareImages}>Compare Images</button>
-      {comparisonResult && <p>Comparison Result: {comparisonResult}</p>}
+      <div className="container">
+        <div className="left-panel">
+          <div className="gallery">
+            {images.map((url, index) => (
+              <div
+                className={`image-container ${
+                  url === image1 ? "selected-image1" : ""
+                } ${url === image2 ? "selected-image2" : ""}`}
+                key={index}
+                onClick={() => handleImageClick(url)}
+                style={
+                  index === images.length - 1
+                    ? {
+                        marginBottom: "120px",
+                      }
+                    : {}
+                }
+              >
+                <img src={url} alt={`img-${index}`} className="image" />
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="right-panel">
+          <div className="selected-images">
+            <div
+              className={`selected-image-container ${
+                image1 ? "selected-image1" : ""
+              }`}
+            >
+              {image1 ? (
+                <img src={image1} alt="Selected 1" className="image" />
+              ) : (
+                <div className="placeholder">Select Image 1</div>
+              )}
+
+              {comparisonResult?.boundingBox && (
+                <div
+                  className="bounding-box"
+                  style={{
+                    top: `${comparisonResult?.boundingBox.top * 100}%`,
+                    left: `${comparisonResult?.boundingBox.left * 100}%`,
+                    height: `${comparisonResult?.boundingBox.height * 100}%`,
+                    width: `${comparisonResult?.boundingBox.width * 100}%`,
+                  }}
+                ></div>
+              )}
+            </div>
+
+            <div
+              className={`selected-image-container ${
+                image2 ? "selected-image2" : ""
+              }`}
+            >
+              {image2 ? (
+                <img src={image2} alt="Selected 2" className="image" />
+              ) : (
+                <div className="placeholder">Select Image 2</div>
+              )}
+            </div>
+          </div>
+
+          <div>
+            <button
+              disabled={!image1 || !image2 || loading}
+              onClick={handleCompareImages}
+            >
+              {loading ? "Comparing..." : "Compare Images"}
+            </button>
+            {comparisonResult !== null && (
+              <p>Comparison Result: {comparisonResult?.per}</p>
+            )}
+          </div>
+        </div>
+      </div>
+      {/* Modal for selecting image */}
+      {selectedImage && (
+        <div className="modal">
+          <div className="modal-content">
+            <p>Set the selected image as:</p>
+            <label>
+              <input
+                type="radio"
+                name="setImage"
+                value="image1"
+                onChange={() => setSelectedImageType("image1")}
+                checked={selectedImageType === "image1"}
+              />
+              Image 1
+            </label>
+            <label>
+              <input
+                type="radio"
+                name="setImage"
+                value="image2"
+                onChange={() => setSelectedImageType("image2")}
+                checked={selectedImageType === "image2"}
+              />
+              Image 2
+            </label>
+            <button onClick={() => handleSetImage(selectedImage)}>Yes</button>
+            <button onClick={() => setSelectedImage(null)}>Cancel</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
